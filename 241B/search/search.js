5ce440a004c65b05f33c1fbf403de4b692cf953a
Jenkins <jenkins@ip-172-31-27-202.us-west-2.compute.internal>
2015-10-07 16:46:30 +0000

    Jenkins Doxygen-241B: Docs Updated

--- 241B/search/search.js
+++ 241B/search/search.js
@@ -9,16 +9,14 @@ var indexSectionsWithContent =
 {
   0: "abcdflmprstu",
   1: "fm",
-  2: "abcdflprstu",
-  3: "bf"
+  2: "abcdflprstu"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions",
-  3: "variables"
+  2: "functions"
 };
 
 function convertToId(search)
