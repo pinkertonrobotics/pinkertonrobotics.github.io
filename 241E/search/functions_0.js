08619f18937529eb46046775b96be2068f3686ab
Jenkins <jenkins@ip-172-31-27-202.us-west-2.compute.internal>
2015-10-22 21:15:01 +0000

    Jenkins Doxygen-241E: Docs Updated

--- 241E/search/functions_0.js
+++ 241E/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['auto_5fdrive_5fforward',['auto_drive_forward',['../functions_8c.html#ae67c9c1709bc290560a6337943c649cc',1,'functions.c']]],
   ['autonomous',['autonomous',['../main_8c.html#a371bbbbc61aa430884cb77eb56b3df53',1,'main.c']]]
 ];
--- 241E/search/functions_3.js
+++ 241E/search/functions_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['drive_5fforward',['drive_forward',['../functions_8c.html#af2043f6060fe77e12af48f5ab13923ed',1,'functions.c']]],
   ['drivebyencoder',['driveByEncoder',['../functions_8c.html#a6633d9d8f3bfb3e6e4b05dbbc1599e9c',1,'functions.c']]]
 ];
