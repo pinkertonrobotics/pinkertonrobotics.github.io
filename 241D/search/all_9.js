9ada02d4cbb7f59f6f650008351d49bdf330137f
Jenkins <jenkins@ip-172-31-27-202.us-west-2.compute.internal>
2015-10-29 18:49:38 +0000

    Jenkins Doxygen-241D: Docs Updated

--- 241D/search/all_9.js
+++ 241D/search/all_9.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['usercontrol',['usercontrol',['../main_8c.html#a99a39462aa5b36444a05e4b22d8720b3',1,'main.c']]]
+  ['setmotors',['setMotors',['../functions_8c.html#ad8ed41f25272dc6aad41f3d0faac7dff',1,'functions.c']]],
+  ['setmotorsleft',['setMotorsLeft',['../functions_8c.html#a19e059ac26fceeabb21bf022a931fd0d',1,'functions.c']]],
+  ['setmotorsright',['setMotorsRight',['../functions_8c.html#a0d25f246c45a7aefd6395283e8bed95c',1,'functions.c']]]
 ];
--- 241D/search/functions_7.js
+++ 241D/search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['pre_5fauton',['pre_auton',['../main_8c.html#ab532f0b1474225e81ebd23b2d05f7c11',1,'main.c']]]
+  ['runmotors',['runMotors',['../functions_8c.html#acb22ab198756eb13e6a692e73dc98b3b',1,'functions.c']]]
 ];
