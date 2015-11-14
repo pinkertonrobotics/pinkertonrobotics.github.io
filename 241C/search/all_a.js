29f5c4a768b48b4224861d09ef9c61e021e21f6e
Jenkins <jenkins@ip-172-31-27-202.us-west-2.compute.internal>
2015-11-14 18:34:59 +0000

    Jenkins Doxygen-241C: Docs Updated

--- 241C/search/all_a.js
+++ 241C/search/all_a.js
@@ -4,6 +4,8 @@ var searchData=
   ['timeout_5fcnt_5fper_5fsec',['TIMEOUT_CNT_PER_SEC',['../functions_8c.html#a5359725727b9a0e2588f4790da94b43e',1,'functions.c']]],
   ['turnleftdegrees',['turnLeftDegrees',['../functions_8c.html#aaf5a5fb405343531462600db0961eb04',1,'functions.c']]],
   ['turnleftseconds',['turnLeftSeconds',['../functions_8c.html#a78865695e4268d88e03b81d60c9ce8fb',1,'functions.c']]],
+  ['turnleftticks',['turnLeftTicks',['../functions_8c.html#a7770f47c49b1afb6912978fd649f01c8',1,'functions.c']]],
   ['turnrightdegrees',['turnRightDegrees',['../functions_8c.html#a507906c16b5891f1d2ac958eaae6e734',1,'functions.c']]],
-  ['turnrightseconds',['turnRightSeconds',['../functions_8c.html#a395c992d0024bc009c3b2f2817fab76e',1,'functions.c']]]
+  ['turnrightseconds',['turnRightSeconds',['../functions_8c.html#a395c992d0024bc009c3b2f2817fab76e',1,'functions.c']]],
+  ['turnrightticks',['turnRightTicks',['../functions_8c.html#a9eb766f7a74b0e3ae6cc88eecee55282',1,'functions.c']]]
 ];
