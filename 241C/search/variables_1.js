7cdd79eb79eb1d1c65daa88462453f02bcec6f27
Jenkins <jenkins@ip-172-31-27-202.us-west-2.compute.internal>
2016-02-18 21:05:01 +0000

    Jenkins Doxygen-241C: Docs Updated

--- 241C/search/variables_1.js
+++ 241C/search/variables_1.js
@@ -5,6 +5,6 @@ var searchData=
   ['fw_5fhalf',['FW_half',['../functions_8c.html#a41b997251467cc35215ba6b498b09e29',1,'functions.c']]],
   ['fw_5floopcount',['FW_loopCount',['../functions_8c.html#a32bbafd84e0d68a8afa6c044ba0eb826',1,'functions.c']]],
   ['fw_5fpower',['FW_power',['../functions_8c.html#a5d9b409cc6a78d263dcbd666623d84bf',1,'functions.c']]],
-  ['fw_5fstopped',['FW_stopped',['../functions_8c.html#aa69edd24eb3588f56ae2628dff2a629d',1,'functions.c']]],
+  ['fw_5frunning',['FW_running',['../functions_8c.html#a488ce1ce36db380f4f13290af5dda96b',1,'functions.c']]],
   ['fw_5ftickspassed',['FW_ticksPassed',['../functions_8c.html#aefd09f9da79aa8b03048db90f5929927',1,'functions.c']]]
 ];
